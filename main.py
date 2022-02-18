66a156b8e019e17743f93c29a3f2de079df936e2
Gyobeom <ikiscc@gmail.com>
2022-02-18 17:43:22 +0900

    2022-02-18 test

--- main.py
+++ main.py
@@ -39,6 +39,7 @@ while game_is_on:
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_is_on = False
         score.game_over()
+    print("HiCherry")
 
 
 
